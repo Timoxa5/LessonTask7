610fb1d9bccb0ca5e5aceaee3ad3f6d794679bfb
Timoxa5 <timoxa230510@gmail.com>
2025-05-15 19:52:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
-@app.get("/calc")
-def calc(op: str, a: float, b: float):
-    if op == "+":
-        return {"result": a + b}
-    if op == "-":
-        return {"result": a - b}
-    return {"error": 404}
+# Завдання №1
+@app.get("/users/{user_id}")
+def get_user(user_id: int = Path(..., description="ID користувача")):
+    return {"user_id": user_id}
+
+# Завдання №2
+@app.get("/profile")
+def get_profile(
+    age: int = Query(0, description="Вік (за замовчуванням 0)"),
+    nickname: str = Query(..., description="Нікнейм")
+):
+    return {"age": age, "nickname": nickname}
+
+# Завдання №3
+@app.get("/products/{product_id}")
+def get_product_with_discount(
+    product_id: int = Path(..., description="ID продукту"),
+    discount: float = Query(..., description="Знижка у %")
+):
+    return {"product_id": product_id, "discount": discount}
